3c505cb0d1686147c9f5f7fc42d2c738cd5cd8dc
Suppe <Suppe04@gmail.com>
2024-04-22 15:09:07 +0200

    Database Update and Merge with Main

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,38 @@ import { EventBus } from './game/EventBus';
 
 function App()
 {
+
+            // State to track whether the registration scene button is clicked
+    const [gameEnd, setGameEnd] = useState(true);
+
+    // State to track whether the game button is clicked
+    const [gameStarted, setGameStarted] = useState(false);
+
+    useEffect(() => {
+        // Function to handle event emitted from Phaser
+        const handlePhaserEvent = (value: boolean) => {
+            setGameEnd(value);
+        };
+
+        // Listen for the event emitted from Phaser
+        EventBus.on('booleanValue', handlePhaserEvent);
+
+        // Clean up the event listener when component unmounts
+        return () => {
+            EventBus.off('booleanValue', handlePhaserEvent);
+        };
+    }, []); // Empty dependency array means this effect will run once after the initial render
+
+    // Function to handle game button click
+    const handleGameButtonClick = (isClicked: boolean) => {
+        setGameStarted(isClicked)
+    };
+
+    const handlePlayAgain = (state: boolean) => {
+        setGameStarted(state);
+        setGameEnd(!state);
+    }
+
     //  References to the PhaserGame component (game and scene are exposed)
     const phaserRef = useRef<IRefPhaserGame | null>(null);
 
