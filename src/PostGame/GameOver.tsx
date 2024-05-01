9aa28c4a8519be32e0a2bd7ce46ead8115d68192
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-05-01 22:47:23 +0200

    Added score to gameover page + font for Phaser distance score

--- src/PostGame/GameOver.tsx
+++ src/PostGame/GameOver.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
+import { EventBus } from "../EventBus";
 
+var score = "";
+// Subscribe to score updates
+EventBus.on("score", (data: number) => {
+    score = data.toString();
+});
 
 function GameOver() {
 
@@ -12,6 +18,12 @@ function GameOver() {
                 <div id="gameover">
                     <h2 id="subtitle3">GAME OVER</h2>
                         <div className='mainfont'>Aw man! That's just unlucky.</div>
+                        <br></br>
+                        <br></br>
+                        <div>
+                            <p id="highscore_element" className="scoreTitle">Your Score</p>
+                            <p className="scoreText">{score}</p>
+                        </div>
                         <img src="assets/crashedcar.png" className='fp-car-picture2'></img>
                         <div className='mainfont'>Want to see how far you got?</div>
                         <div className='container'>
