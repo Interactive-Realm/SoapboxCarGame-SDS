1077cdc9b8495fdc559364cd1b51f33e1dd0ed7f
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-05-01 22:10:55 +0200

    GameOver screen fixes. Added intro text

--- src/PostGame/GameOver.tsx
+++ src/PostGame/GameOver.tsx
@@ -10,9 +10,9 @@ function GameOver() {
             <img src="/assets/is-logo.png" alt="IS Logo" className='islogo'></img>
             </div>
                 <div id="gameover">
-                    <h2 id="subtitle3"> SIGN UP</h2>
+                    <h2 id="subtitle3">GAME OVER</h2>
                         <div className='mainfont'>Aw man! That's just unlucky.</div>
-                        <img src="assets/crashedcar.png" className='fp-car-picture'></img>
+                        <img src="assets/crashedcar.png" className='fp-car-picture2'></img>
                         <div className='mainfont'>Want to see how far you got?</div>
                         <div className='container'>
                             <button className='buttonwhite mainfont'>Leaderboard</button>
