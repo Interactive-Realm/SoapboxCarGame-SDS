d5ea2805b7dc1fe7a965484bc79db1fcd706ed0a
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-05-01 21:58:13 +0200

    component ready for review

--- src/PostGame/GameOver.tsx
+++ src/PostGame/GameOver.tsx
@@ -9,7 +9,16 @@ function GameOver() {
             <div>
             <img src="/assets/is-logo.png" alt="IS Logo" className='islogo'></img>
             </div>
-            
+                <div id="gameover">
+                    <h2 id="subtitle3"> SIGN UP</h2>
+                        <div className='mainfont'>Aw man! That's just unlucky.</div>
+                        <img src="assets/crashedcar.png" className='fp-car-picture'></img>
+                        <div className='mainfont'>Want to see how far you got?</div>
+                        <div className='container'>
+                            <button className='buttonwhite mainfont'>Leaderboard</button>
+                        </div>
+                </div>
+                    
         </div>
     );
 }
